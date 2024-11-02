e30ee74d6636b30b92953e9acf6243570b221038
sumonpaul55 <sumpaul100@gmail.com>
2024-11-02 13:49:00 +0600

    filter and maping remaingin is completed

---
+++ ArrayFilteringAndMapping.js
@@ -0,0 +1,12 @@
+const people = [
+    { name: 'John', age: 30, gender: 'male' },
+    { name: 'Jane', age: 25, gender: 'female' },
+    { name: 'Mike', age: 40, gender: 'male' },
+    { name: 'Emma', age: 35, gender: 'female' },
+    { name: 'Tom', age: 20, gender: 'male' }
+];
+
+
+const arrayOfname = people.filter(item => item.gender !== "female").map(item => item.name)
+
+console.log(arrayOfname)
